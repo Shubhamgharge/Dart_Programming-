7d8866fbe9948a05f4373a7d2d58d54520b1193a
Shubham Gharge <159037288+Shubhamgharge@users.noreply.github.com>
2025-05-18 08:25:34 +0530

    Add files via upload

---
+++ list anid map/001 list.dart
@@ -0,0 +1,12 @@
+
+
+// Demo OF List
+void main() {
+  List name = ['shubham', 'sahil', 'pranav', 'parth'];
+
+  for (int i = 0; i < name.length; i++) print(name[i]);
+
+  print(name);
+
+  print(name[3]);
+}
